54754c6aa76350e281aabfa52b2ad3a59c4c42aa
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-12 20:55:24 +0100

    Navbar component is done

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,79 @@
 import React, { useState } from "react";
 import { Avatar } from "@mui/material";
+import { GoogleLogout } from "react-google-login";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectSignedIn,
+  selectUserData,
+  setSignedIn,
+  setUserData,
+  setSearchInput,
+} from "../Features/userSlice";
 
 import "../Styles/navbar.css";
 
 const NavBar = () => {
   const [inputValue, setInputValue] = useState("tech");
-  return <div>Navbar</div>;
+
+  const isSignedIn = useSelector(selectSignedIn);
+
+  const userData = useSelector(selectUserData);
+
+  const dispatch = useDispatch();
+
+  const logout = (reponse) => {
+    dispatch(setSignedIn(true));
+    dispatch(setSearchInput(inputValue));
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    dispatch(setInputValue());
+  };
+  return (
+    <div className="navbar">
+      <h1 className="navbar-header">BlogMania 💬</h1>
+      {isSignedIn && (
+        <div className="blog-search">
+          <input
+            className="search"
+            placeholder="Search for a blog"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button className="submit" onClick={handleClick}>
+            Search
+          </button>
+        </div>
+      )}
+
+      {isSignedIn ? (
+        <div className="navbar-user-data">
+          <Avatar
+            className="user"
+            src={userData?.imageUrl}
+            alt={userData?.name}
+          />
+          <h1 className="signedIn">{userData?.givenName}</h1>
+          <GoogleLogout
+            buttonText="Logout 😦"
+            render={(renderProps) => (
+              <button
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                className="logout-button"
+              >
+                Logout 😦
+              </button>
+            )}
+            onLogoutSuccess={logout}
+          />
+        </div>
+      ) : (
+        <h1 className="notSignedIn">User not available 😞</h1>
+      )}
+    </div>
+  );
 };
 
 export default NavBar;
